b761594973a926ce74f845de04143625f5959423
Konouz1 <konouz@finic.nl>
2023-10-29 01:19:11 +0300

    event api eventsGrouped an add image

--- app/Http/Controllers/InfoAPIController.php
+++ app/Http/Controllers/InfoAPIController.php
@@ -20,43 +20,41 @@ class InfoAPIController extends Controller
     
         $eventsGrouped = [];
         $yearCounter = 1; // Initialize the year counter
-        $images = [];
+    
         foreach ($events_Info as $event) {
             $year = $event->event_year;
-            $formattedEvents = $events_Info
-                ->where('event_year', $year)
-                ->map(function ($event) {
-                    $date = Carbon::parse($event->date);
-                    $formattedDate = $date->format('F d, Y');
-                    $month = $date->format('F');
-                    $images[] = $event->image;
-
-                    return [
-                        'id' => $event->id,
-                        'event_name' => $event->title,
-                        'date' => $formattedDate,
-                        'month' => $month,
-                        'image' => $event->image,
-                        'description' => $event->description,
-                        'is_flagged' => $event->priority_level === 1,
-                        'resource_links' => $event->resourceLinks,
-                    ];
-                });
     
-            $eventsGrouped[] = [
-                'id' => $yearCounter, // Assign the counter as an id
-                'img' => $images,
-                'date_year' => $year,
-                'events' => $formattedEvents->unique('id')->values()->all(),
+            if (!isset($eventsGrouped[$year])) {
+                $eventsGrouped[$year] = [
+                    'id' => $yearCounter, // Assign the counter as an id
+                    'img' => [], // Initialize an empty array for images
+                    'date_year' => $year,
+                    'events' => [], // Initialize an empty array for events
+                ];
+                $yearCounter++; // Increment the year counter
+            }
+    
+            $date = Carbon::parse($event->date);
+            $formattedDate = $date->format('F d, Y');
+            $month = $date->format('F');
+            $eventsGrouped[$year]['img'][] = $event->image; // Add image to the year's images array
+    
+            $eventsGrouped[$year]['events'][] = [
+                'id' => $event->id,
+                'event_name' => $event->title,
+                'date' => $formattedDate,
+                'month' => $month,
+                'image' => $event->image,
+                'description' => $event->description,
+                'is_flagged' => $event->priority_level === 1,
+                'resource_links' => $event->resourceLinks,
             ];
-    
-            $yearCounter++; // Increment the year counter
         }
     
-        // Make sure the year is unique and not repeated
-        $uniqueYears = collect($eventsGrouped)->unique('date_year')->values()->all();
+        // Re-index the array so that the IDs start from 1
+        $eventsGrouped = array_values($eventsGrouped);
     
-        return response()->json($uniqueYears);
+        return response()->json($eventsGrouped);
     }
     
     
