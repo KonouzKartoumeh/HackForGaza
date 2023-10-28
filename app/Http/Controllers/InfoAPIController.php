3d292dc76af237ef30ac05195b954e163df63377
Konouz1 <konouz@finic.nl>
2023-10-28 22:44:02 +0300

    info API v1

--- app/Http/Controllers/InfoAPIController.php
+++ app/Http/Controllers/InfoAPIController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 use App\Http\Controllers\Controller;
 use App\Models\EventInfo;
 use App\Models\ResourceLink;
@@ -12,23 +13,43 @@ class InfoAPIController extends Controller
 {
     public function index()
     {
-
         $events_Info = EventInfo::with('resourceLinks')
+        ->select('id', 'title', 'date', 'event_year', 'image', 'description', 'priority_level')
+        ->distinct('event_year')
         ->latest()
-        ->get()
-        ->map(function ($event) {
+        ->get();
+    
+    
+    $eventsByYear = [];
+    foreach ($events_Info as $event) {
+        $year = $event->event_year;
+        $events = EventInfo::where('event_year', $year)->get();
+    
+        $formattedEvents = $events->map(function ($event) {
+            $date = Carbon::parse($event->date); 
+            $formattedDate = $date->format('F d, Y'); 
+            $month = $date->format('F'); 
+    
             return [
                 'id' => $event->id,
-                'title' => $event->title,
-                'date' => $event->date,
-                'year' => $event->event_year,
+                'event_name' => $event->title,
+                'date' => $formattedDate, 
+                'month' => $month, 
                 'image' => $event->image,
-                'description' => $event->description,                
-                'is_flagged' => $event->priority_level === 1 ? true : false,
+                'description' => $event->description,
+                'is_flagged' => $event->priority_level === 1,
                 'resource_links' => $event->resourceLinks,
             ];
         });
     
-    return response()->json($events_Info);
+        $eventsByYear[] = [
+            'date_year' => $year,
+            'events' => $formattedEvents,
+        ];
     }
+    
+    return response()->json($eventsByYear);
+    }
+    
+    
 }
